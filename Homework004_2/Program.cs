39aea96b069a9187e4ef48106dd5b7dc46000ebd
Pavel <ierpvk@gmail.com>
2022-07-24 20:26:37 +0500

    Homework004_2 project

---
+++ Homework004_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.Console.WriteLine("введите число");
+
+Console.WriteLine("введите число");
+int Number = int.Parse(Console.ReadLine());
+int sum = 0;
+while (Number > 0)
+{
+int num = Number % 10;
+Number = Number / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна ->" + sum);
