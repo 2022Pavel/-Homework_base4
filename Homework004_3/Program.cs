bd7566ffd76330f27cafe3e196bd693a4b48e1d9
Pavel <ierpvk@gmail.com>
2022-07-26 12:10:20 +0500

    Homework004_3 project

---
+++ Homework004_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+
+for(int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"\nВведите количество элементов массива под индексом {i}:\t");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+Console.Write($"\nВыведите массив ->" );
+for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i]);
+    }
